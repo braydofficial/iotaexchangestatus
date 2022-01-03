168459e451971a6d0f6d10f7c1b97391e35bd5bc
braydofficial <ramorak@canreal.net>
2022-01-03 12:16:10 +0100

    Added sample config

---
+++ sample-config.php
@@ -0,0 +1,9 @@
+<?php
+    // Copy this file to config.php and fill it.
+    // DO NOT PUT YOUR CREDENTIALS IN HERE AND PUSH TO THE REPO OR THEY ARE GETTING EXPOSED!
+    $host = "localhost";
+    $database = "db";
+    $username = "user";
+    $password = "pass";
+    $IPSalt = "SALT"; // Change to set custom salt to protect IP addresses in case of database breach
+?>
