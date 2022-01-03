1268056ba60449e03a91df5283e9c81ae9b37b78
thisdudeisvegan <hallo@byronfroehlich.xyz>
2022-01-03 18:49:12 +0100

    Testing IOTAphp Lib

--- functions/getAddressBalance.php
+++ functions/getAddressBalance.php
@@ -6,6 +6,8 @@
 
     $client = new SingleNodeClient();
 
-    echo $ret = (new getBalance($client))->address('60200bad8137a704216e84f8f9acfe65b972d9f4155becb4815282b03cef99fe')
-    ->run();
+    function getAddressBalance($string) {
+        $address = $string;
+        echo $ret = (new getBalance($client))->address('$address')->run();
+    }
 ?>
---
+++ functions/test.php
@@ -0,0 +1,4 @@
+<?php
+    require_once('getAddressBalance.php');
+    getAddressBalance("iota1qqm9k3003sszpq4d5n0yc89pk6dvpm3wvc7lexlzr2nwwrpaxaxdg7f85k6");
+?>
