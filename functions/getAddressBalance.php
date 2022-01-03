66a355b9730c41bf3aa5a19e98938851010daff1
thisdudeisvegan <hallo@byronfroehlich.xyz>
2022-01-03 19:42:47 +0100

    Enable conversion of IOTA balance

--- functions/getAddressBalance.php
+++ functions/getAddressBalance.php
@@ -4,20 +4,34 @@
     use IOTA\Client\SingleNodeClient;
     use IOTA\Action\getBalance;
     function getAddressBalance($string) {
-        
-
+        // Get address data via iota.php
         $client = new SingleNodeClient('mainnet');
         $address = $string;
         $ret = $client->address($address);
+        // JSON Decode IOTA address data
         $ret = json_decode($ret, true);
         $iota = $ret['balance'];
         
-        // Check which type it is
+        // Check which type it is, convert and return
         if($iota >= 0 && $iota <= 999999) {
-            echo "$iota IOTA";
+            $res = "$iota IOTA";
+            return $res;
         } elseif($iota >= 1000000 && $iota <= 999999999) {
             $miota = $iota / 1000000;
-            echo "$miota MIOTA";
+            $res = "$miota MIOTA";
+            return $res;
+        } elseif($iota >= 1000000000 && $iota <= 999999999999) {
+            $giota = $iota / 1000000000;
+            $res = "$giota GIOTA";
+            return $res;
+        } elseif($iota >= 1000000000000 && $iota <= 999999999999999) {
+            $tiota = $iota / 1000000000000;
+            $res = "$tiota TIOTA";
+            return $res;
+        } elseif($iota <= 1000000000000000) {
+            $piota = $iota / 1000000000000000;
+            $res = "$piota PIOTA";
+            return $res;
         }
     }
 ?>
