3434effdbc09aabb7bf1909f43ca9135e1b0f08f
thisdudeisvegan <hallo@byronfroehlich.xyz>
2022-01-03 18:55:24 +0100

    Testing IOTAphp Lib

--- functions/test.php
+++ functions/test.php
@@ -2,5 +2,5 @@
     ini_set('display_errors', 1);
     error_reporting(~0);
     require_once("getAddressBalance.php");
-    getAddressBalance("iota1qqm9k3003sszpq4d5n0yc89pk6dvpm3wvc7lexlzr2nwwrpaxaxdg7f85k6");
+    getAddressBalance("qqm9k3003sszpq4d5n0yc89pk6dvpm3wvc7lexlzr2nwwrpaxaxdg7f85k6");
 ?>
