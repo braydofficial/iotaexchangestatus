838eadb7aab9be559dbf12eb0e731508a1ff0c25
thisdudeisvegan <hallo@byronfroehlich.xyz>
2022-01-03 19:55:29 +0100

    Added Binance wallet balances

--- functions/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-    ini_set('display_errors', 1);
-    error_reporting(~0);
-    require_once("getAddressBalance.php");
-    echo getAddressBalance("iota1qrp7wn7ag964yh7r6csphg8l47q9ehlpxz8tukhad09x7avjj09s5qunq87");
-?>
