d9727b52596676a144eb597d76c7da89a61101df
thisdudeisvegan <hallo@byronfroehlich.xyz>
2022-01-03 18:43:52 +0100

    Testing IOTAphp Lib

--- functions/getAddressBalance.php
+++ functions/getAddressBalance.php
@@ -1,7 +1,11 @@
 <?php
-    $path = "../iota.php/autoload.php";
-    require_once($path);
-    $client = new IOTA\Client\SingleNodeClient();
+    require_once("../iota.php/autoload.php");
+    
+    use IOTA\Client\SingleNodeClient;
+    use IOTA\Action\getBalance;
 
-    echo $client->info();
+    $client = new SingleNodeClient();
+
+    echo $ret = (new getBalance($client))->address('60200bad8137a704216e84f8f9acfe65b972d9f4155becb4815282b03cef99fe')
+    ->run();
 ?>
