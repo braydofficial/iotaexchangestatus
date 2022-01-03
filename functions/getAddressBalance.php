b6e4546352aa022f8a911372483f83a13f06014e
braydofficial <ramorak@canreal.net>
2022-01-03 14:58:03 +0100

    Add IOTA.php for testing

--- functions/getAddressBalance.php
+++ functions/getAddressBalance.php
@@ -1,6 +1,9 @@
 <?php
-    require_once("iota.php\autoload.php");
-    $client = new iota.php\IOTA\Client\SingleNodeClient();
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/iota.php/autoload.php";
+    require_once($path
+);
+    $client = new IOTA\Client\SingleNodeClient();
 
     echo $client->info();
 ?>
