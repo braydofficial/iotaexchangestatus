c106f2eaeada9f7f46a0f2dbf56c6de9d590d2a7
thisdudeisvegan <hallo@byronfroehlich.xyz>
2022-01-03 19:44:50 +0100

    Balance test with Binance hot wallet

--- functions/test.php
+++ functions/test.php
@@ -2,5 +2,5 @@
     ini_set('display_errors', 1);
     error_reporting(~0);
     require_once("getAddressBalance.php");
-    getAddressBalance("iota1qrp7wn7ag964yh7r6csphg8l47q9ehlpxz8tukhad09x7avjj09s5qunq87");
+    echo getAddressBalance("iota1qrp7wn7ag964yh7r6csphg8l47q9ehlpxz8tukhad09x7avjj09s5qunq87");
 ?>
